b8a10019261e34ff3e29fea25665d010d53d7b2b
hellomon2001 <devwebtannguyen@gmail.com>
2024-07-05 03:13:47 +0700

    new update: done menubar

--- scrips/renderMenuBar.js
+++ scrips/renderMenuBar.js
@@ -1,3 +1,25 @@
-function renderMenuBar() {
-    
+const toggleButton = document.querySelector("#toggleButton")
+const toggleMenu = document.querySelector(".toggleMenu")
+const overlay = document.querySelector(".overlay")
+const contain_menu = document.querySelector(".contain_menu")
+const close_menu = document.querySelector(".close_menu")
+
+toggleButton.addEventListener("click", () =>{
+    if (toggleMenu.classList.contains("hidden")) {
+        toggleMenu.classList.remove("hidden")
+    }
+    else {
+        toggleMenu.classList.add("hidden")
+    }
+})
+
+const hidden = () =>{
+    toggleMenu.classList.add("hidden")
 }
+
+close_menu.addEventListener("click", hidden)
+contain_menu.addEventListener("click", hidden)
+
+overlay.addEventListener("click", (e) =>{
+    e.stopPropagation()
+})
