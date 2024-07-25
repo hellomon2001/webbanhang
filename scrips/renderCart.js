419e5db547c59ba086b60476ff85117c4aee2764
hellomon2001 <devwebtannguyen@gmail.com>
2024-07-26 03:23:45 +0700

    update: done commit

--- scrips/renderCart.js
+++ scrips/renderCart.js
@@ -1,10 +1,7 @@
 const hidden_cart = document.querySelector(".hidden_cart")
 const cart = document.querySelector(".cart")
-<<<<<<< HEAD
 
 
-=======
->>>>>>> f69041ca99fd5ef8960729182e6700032a9fce4a
 
 hidden_cart.addEventListener("mouseenter", () =>{
     cart.classList.remove("obscure");
@@ -21,9 +18,5 @@ const showCart = () => {
     cart.classList.remove("obscure");
 };
 
-<<<<<<< HEAD
-cart.addEventListener("mouseenter", showCart)
-=======
 cart.addEventListener("mouseenter", showCart)
 cart.addEventListener("mouseleave", obscure)
->>>>>>> f69041ca99fd5ef8960729182e6700032a9fce4a
